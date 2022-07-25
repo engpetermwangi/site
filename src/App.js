9611a49bd3e27e59b5a360a83cfeed339180f97e
engpetermwangi <pmwangi155@gmail.com>
2022-07-25 19:23:02 +0300

    Planetary links and styling update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+import "./App.css";
 
-function App() {
+import peter from "./pics/peter.jpg";
+import githubLogo from "./pics/github.png";
+import linkedinLogo from "./pics/linkedin.png";
+import codewarsLogo from "./pics/codewars.png";
+import codesandboxLogo from "./pics/codesandbox.svg";
+import resume from "./pics/resume.svg";
+
+const links = [
+  {
+    link: "https://github.com/engpetermwangi",
+    imageSrc: githubLogo,
+    imageAlt: "GitHub logo",
+    offset: Math.PI / 2,
+  },
+  {
+    link: "https://www.linkedin.com/in/peter-mwangi-59932bb9/",
+    imageSrc: linkedinLogo,
+    imageAlt: "LinkedIn logo",
+    offset: (9 / 10) * Math.PI,
+  },
+  {
+    link: "https://www.codewars.com/users/engpetermwangi",
+    imageSrc: codewarsLogo,
+    imageAlt: "Codewars logo",
+    offset: (1 / 10) * Math.PI,
+  },
+  {
+    link: "https://codesandbox.io/u/engpetermwangi",
+    imageSrc: codesandboxLogo,
+    imageAlt: "CodeSandbox logo",
+    offset: (13 / 10) * Math.PI,
+  },
+  {
+    link: "https://petermwangi.me/resume",
+    imageSrc: resume,
+    imageAlt: "Resume",
+    offset: (-3 / 10) * Math.PI,
+  },
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <img src={peter} alt="Peter" />
+      {links.map((linkProps) => (
+        <AnimatedLink key={linkProps.link} {...linkProps} />
+      ))}
     </div>
   );
-}
+};
+
+const AnimatedLink = ({ link, imageSrc, imageAlt, offset }) => {
+  const angle = useMotionValue(offset);
+
+  useEffect(() => {
+    const controls = animate(angle, 2 * Math.PI + offset, {
+      duration: 24,
+      repeat: Infinity,
+      ease: "linear",
+    });
+    return controls.stop;
+  }, [angle, offset]);
+
+  const x = useTransform(angle, (value) => 180 * Math.cos(value));
+  const y = useTransform(angle, (value) => 180 * (1 - Math.sin(value)));
+
+  return (
+    <motion.a href={link} className="link" style={{ x, y }} target="_blank">
+      <img src={imageSrc} alt={imageAlt} />
+    </motion.a>
+  );
+};
 
 export default App;
